bf357421a47d969672749a426dcdf4570ea9c94f
Abdulrahman1890 <abdulrahmanmm2011@hotmail.com>
2023-06-03 05:55:22 +0300

    Card of the product and Home Fragment Implementation

--- app/src/main/java/com/example/product_viewer/home/HomeAdapter.kt
+++ app/src/main/java/com/example/product_viewer/home/HomeAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.product_viewer.home
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.data.repo.roomDB.entity.ProductEntity
+import com.example.product_viewer.databinding.CardProductBinding
+
+
+class HomeAdapter(private val products: MutableList<ProductEntity>) :
+    RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+    inner class ViewHolder(private val binding: CardProductBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(product: ProductEntity) {
+
+            binding.product = product
+
+            binding.root.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToDetailsFragment(product.id)
+                binding.root.findNavController().navigate(action)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CardProductBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = products[position]
+        holder.bind(product)
+    }
+
+    fun updateList(products: List<ProductEntity>) {
+        this.products.clear()
+        this.products.addAll(products)
+        notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/com/example/product_viewer/home/HomeViewModel.kt
+++ app/src/main/java/com/example/product_viewer/home/HomeViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.product_viewer.home
 
-class HomeViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.repo.RepoImpl
+import com.example.data.repo.roomDB.ProductDataBase
+import kotlinx.coroutines.launch
+
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = ProductDataBase.getInstance(application.applicationContext)
+    private val repo = RepoImpl(database)
+
+    init {
+        viewModelScope.launch {
+            repo.refreshProducts()
+        }
+    }
+
+    val product = repo.product
 }
